97d4b238039654e68e25785265298891897c2aa3
Ethan Ho <ethan.j.ho-1@ou.edu>
2021-07-17 20:59:54 -0500

    Create script to build files

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+STATIC_DIR=./static
+
+# Install dependencies
+if [ ! -d "$STATIC_DIR/node_modules/" ]; then
+    npm --prefix $STATIC_DIR install $STATIC_DIR
+
+# Run build script
+npm run --prefix $STATIC_DIR build
