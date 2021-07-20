42e4d0efac4cb94c4834d9cb0e29f51544842801
Ethan Ho <ethan.j.ho-1@ou.edu>
2021-07-19 20:00:09 -0500

    Fix build script typo with fi

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ STATIC_DIR=./static
 # Install dependencies
 if [ ! -d "$STATIC_DIR/node_modules/" ]; then
     npm --prefix $STATIC_DIR install $STATIC_DIR
+fi
 
 # Run build script
 npm run --prefix $STATIC_DIR build
